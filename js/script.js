960ef5ffc973313ad1257ddfe3d8c037058ada45
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-06-18 16:40:58 +0200

    Closing project: add user input add controls for user input

--- js/script.js
+++ js/script.js
@@ -4,33 +4,55 @@
 
 //* creo un array e lo riempio con 'numeriDaCreare' numeri random
 const arrayNumeri = [];
-const numeriDaCreare = 10;
+let numeriDaCreare = parseInt(prompt('Inserisci un numero maggiore di 0'));
+while(isNaN(numeriDaCreare) || numeriDaCreare <= 0){
+    alert('ATTENZIONE! devi inserire solo un numero che sia maggiore di 0');
+    numeriDaCreare = parseInt(prompt('Inserisci un numero'));
+}
+
+//* funzione per ricavare numeri random da 1 a 100
 const randomNum = (min, max) => Math.floor(Math.random() * (max - min + 1) ) + min;
 
 while (arrayNumeri.length < numeriDaCreare){
-    arrayNumeri.push(randomNum(1,100));
+    let numeroRandom = randomNum(1,100);
+    if(!arrayNumeri.includes(numeroRandom)){
+        arrayNumeri.push(numeroRandom);
+    }
 }
-
 console.log(arrayNumeri);
 console.log('*********');
 
+//* imposto due valori di minimo e massimo
+let minVal = parseInt(prompt('Inserisci un numero da 0 a ' + (arrayNumeri.length - 1)));
+while(isNaN(minVal) || minVal < 0 || minVal >= arrayNumeri.length){
+    alert('ATTENZIONE! devi inserire solo un numero che sia compreso tra 0 e ' + (arrayNumeri.length - 1));
+    minVal = parseInt(prompt('Inserisci un numero da 0 a ' + (arrayNumeri.length - 1)));
+}
 
-const minVal = 1;
-const maxVal = 5;
+let maxVal = parseInt(prompt('Inserisci un numero da ' + minVal + ' a ' + (arrayNumeri.length - 1)));
+while(isNaN(maxVal) || maxVal < minVal || maxVal >= arrayNumeri.length){
+    alert('ATTENZIONE! devi inserire solo un numero che sia compreso tra ' + minVal + ' e ' + (arrayNumeri.length - 1));
+    maxVal = parseInt(prompt('Inserisci un numero da ' + minVal + ' a ' + (arrayNumeri.length - 1)));
+}
 
-let arrayFiltered = [];
 
 //* soluzione con forEach
-arrayNumeri.forEach((element,index) => {
-    if(index >= minVal && index <= maxVal){
-        arrayFiltered.push(element);
-    }
-});
+const forEachMethod = (arrayIn,a,b) => {
+    let arrayFiltered = [];
+    arrayIn.forEach((element,index) => {
+        if(index >= a && index <= b){
+            arrayFiltered.push(element);
+        }
+    });
+    return arrayFiltered;
+};
 console.log('Soluzione fatta con forEach');
-console.log(arrayFiltered);
+console.log(forEachMethod(arrayNumeri,minVal,maxVal));
 
 
 //* soluzione con filter
-arrayFiltered = arrayNumeri.filter((element,index) => index >= minVal && index <= maxVal);
+const filterMethod = (arrayIn,a,b) => {
+    return arrayIn.filter((element,index) => index >= a && index <= b);
+};
 console.log('Soluzione fatta con filter');
-console.log(arrayFiltered);
+console.log(filterMethod(arrayNumeri,minVal,maxVal));
